38295aeabae645f914a3dd9b4c6b2d6f367d828c
ToothieWn <tasuke8@gmail.com>
2024-10-27 18:57:42 +0900

    page126

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
     class HelloController extends Controller
     {
         public function index(Request $request) {
-            return view('hello.index');
+            return view('hello.index',['msg'=>'フォームを入力: ']);
+        }
+
+        public function post(Request $request) {
+            $validate_rule = $request->validate([ //$request->validate([配列でvalidate])は新しい書き方。もともとは変数名 = [];で、下の$this->validateだった。
+                'name' => 'required',
+                'mail' => 'email',
+                'age' => 'numeric|between:0,150',
+            ]);
+            // $this->validate($request,$validate_rule);古い書き方
+            return view('hello.index',['msg'=>'正しく入力されました']);
         }
     }
