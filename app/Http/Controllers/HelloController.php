180812f790a46d414f839bf850414da11df67940
ToothieWn <tasuke8@gmail.com>
2024-10-28 10:16:08 +0900

    page145

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,14 +5,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest; // 142pページで追加
-
+use Validator; // 144ページで追加
     class HelloController extends Controller
     {
         public function index(Request $request) {
             return view('hello.index',['msg'=>'フォームを入力: ']);
         }
 
-        public function post(HelloRequest $request) {
+        public function post(Request $request) {
+            $validator = Validator::make($request->all(),[
+                'name' => 'required',
+                'mail' => 'email',
+                'age' => 'numeric|between:0,150',
+            ]);
+            if($validator->fails()) {
+                return redirect('/hello')
+                    ->withErrors($validator)
+                    ->withInput();
+            }
             return view('hello.index',['msg'=>'正しく入力されました']);
         }
     }
