938a3e64f0072f1aae4025ddb6d72bd534551dfd
ToothieWn <tasuke8@gmail.com>
2024-11-10 12:40:22 +0900

    page204

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,31 +7,65 @@ use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest; // 142pページで追加
 use Illuminate\Foundation\Validation\ValidatesRequests; //165ページで追加
 use Validator; // 144ページで追加
+use Illuminate\Support\Facades\DB; // 192ページで追加
+
     class HelloController extends Controller
     {
-        use ValidatesRequests;//165ページで追加
-        //これがないとvalidate()メソッドが使えない
-
         public function index(Request $request) {
-            if($request->hasCookie('msg')) {
-                $msg = 'Cookies: ' . $request->cookie('msg');
-            } else {
-                $msg ='※クッキーはありません。';
-            }
-            return view('hello.index',['msg'=>$msg]);
+            $items = DB::select('SELECT * FROM people');
+            return view('hello.index',['items' => $items]);
         }
 
         public function post(Request $request) {
-            $validate_rule = [
-                'msg' => 'required',
-            ];
-            $this->validate($request,$validate_rule);
-            $msg = $request->msg;
-            $response = response()->view('hello.index',
-            [
-                'msg' => '「' . $msg . '」をクッキーに保存しました。',
-            ]);
-            $response->cookie('msg', $msg, 100);
-            return $response;
+            $items = DB::select('SELECT * FROM people');
+            return view('hello.index',['items' => $items]);
         }
-    }
+        public function add(Request $request) {
+            return view('hello.add');
+        }
+
+        public function create(Request $request) {
+            $param = [
+                'name' => $request->name,
+                'mail' => $request->mail,
+                'age' => $request->age,
+            ];
+
+            DB::insert('INSERT INTO people (name,mail,age) VALUES (:name, :mail, :age)',$param);
+            return redirect('/hello');
+        }
+
+        public function edit(Request $request) {
+            $param = ['id' => $request->id];
+            $item = DB::select('SELECT * FROM people WHERE id = :id',$param);
+            if (empty($item)) {
+                return redirect('/hello')->with('error', 'Record not found.');
+            }
+            return view('hello.edit',['form' => $item[0]]);
+        }
+
+        public function update(Request $request) {
+            $param = [
+                'id' => $request->id,
+                'name' => $request->name,
+                'mail' => $request->mail,
+                'age' => $request->age,
+            ];
+            DB::update('UPDATE people SET name = :name,mail = :mail,age = :age WHERE id = :id',$param);
+            return redirect('/hello');
+        }
+
+        public function del(Request $request) {
+            $param = ['id' => $request->id];
+            $item = DB::select('SELECT * FROM people WHERE id = :id', $param);
+            return view('hello.del',['form' => $item[0]]);
+        }
+
+        public function remove(Request $request) {
+            $param = [
+                'id' => $request->id,
+            ];
+            DB::delete('DELETE FROM people WHERE id = :id',$param);
+            return redirect('/hello');
+        }
+    }  
